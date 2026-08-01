d363ad10f35e82d583050d16d2052ff4d06415d0
princego812 <prince.gopal2005@gmail.com>
2026-08-01 10:50:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,10 +129,10 @@ if (user_input):
     if st.button("Fetch Latest News", key = "News-Button"):
       with st.spinner("Running Agent"):
         try:
-          prompt = """Give Latest News Related to Given user Query 
+          prompt = f"""Give Latest News Related to Given user Query 
           in Dynamic HTML , Output with cards Desgin Format.
           Strict HTML Output , No Any markdowns Response
-          User Query: """user_input
+          User Query: {user_input}"""
           
           response = leader_agent.invoke({'messages':[{'role':'user','content':prompt}]})
           code = response['messages'][-1].content[-1]['text']
